33643f3a9e71ec49c861445be8d9d92cc7823962
Lakshmi Kant Kumar <lakshmikantk506@gmail.com>
2025-11-24 23:35:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
+
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 OWNER_ID = int(os.getenv('OWNER_ID', '0'))
 OWNER_NAME = os.getenv('OWNER_NAME', 'Owner')
@@ -12,3 +14,6 @@ STT_PROVIDER = os.getenv('STT_PROVIDER', 'whisper')
 TTS_PROVIDER = os.getenv('TTS_PROVIDER', 'eleven')
 ADMIN_DASH_TOKEN = os.getenv('ADMIN_DASH_TOKEN', 'changeme_token')
 OWNER_API_URL = os.getenv('OWNER_API_URL', '')
+
+# ⭐ PYROGRAM/ASSISTANT SESSION LOADER
+SESSION = os.getenv('SESSION', '')
